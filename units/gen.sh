e9f46915377358bcb4d8de0b93c6f74423e8d47c
root <root@vast.(none)>
2012-10-24 13:54:47 +0200

    initial import with unit script

---
+++ units/gen.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Generates slightly shadowed transparent png unit icons from btn-unit* DDS
+#
+# Specify size as first argument, final result will be in icons/<size>/
+
+w=$1
+h=$1
+
+mkdir -p icons/$w
+
+for k in dds/*dds; do
+
+	filename=$(basename "$k")
+	extension="${filename##*.}"
+	filename="${filename%.*}"
+	m=`expr match "$filename" '\(btn-unit-[a-zA-Z]*-\)'`
+	unit="${filename//$m/}"
+
+	echo "$k -> icons/$w/$unit.png ($w x $h)"
+
+	convert $k -resize $w\x$h _tmp.png
+	convert _tmp.png -fx "v.p{0,u*v.h}" -channel RGBA -gaussian-blur 0x1 -alpha on -channel RGB -evaluate set 50% +channel _tmpb.png
+	convert \
+		\( _tmpb.png \) \
+		-compose over \
+		\( _tmp.png -unsharp 0x1 \) \
+		-compose over -composite -gravity center icons/$w/$unit.png
+
+#exit
+done
+
+rm -f _tmp.png
+rm -f _tmpb.png
+
