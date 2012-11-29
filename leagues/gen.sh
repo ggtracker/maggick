25b430ec38b597b0af8613a8890070ff5810ed6b
Marian Rudzynski <mr@impaled.org>
2012-11-29 17:43:45 +0100

    added colored race images and glow

--- leagues/gen.sh
+++ leagues/gen.sh
@@ -4,8 +4,7 @@
 # Also creates bw versions (base should be colored)
 # And now also generates all sizes from our 112x112 default
 
-mkdir -p processed
-mkdir -p plain-processed
+mkdir -p output
 
 for w in 16 32 48 64; do
 	h=$w
@@ -18,11 +17,9 @@ for w in 16 32 48 64; do
 
 		echo "$k -> plain/$filename\-$w.png ($w x $h)"
 
-		convert $k -resize $w\x$h plain-processed/$filename\-$w\-color.png
-
-		convert $k -colorspace Gray -black-threshold 90% -contrast -resize $w\x$h plain/$filename\-$w.png
-
-		convert plain/$filename\-$w.png -negate plain/$filename\-$w\-white.png
+		convert $k -resize $w\x$h output/$filename\-color\-$w.png
+		convert $k -colorspace Gray -black-threshold 90% -contrast -resize $w\x$h output/$filename\-$w.png
+		convert output/$filename\-$w.png -negate output/$filename\-white\-$w.png
 	#exit
 	done
 
@@ -35,7 +32,14 @@ for w in 16 32 48 64; do
 
 		echo "$k -> processed/$filename\-$w.png ($w x $h)"
 
-		convert $k -resize $w\x$h processed/$filename\-$w\.png
+		convert $k -resize $w\x$h output/$filename\-mpq\-$w\.png
+                convert $k \( \
+                        +clone -background '#2cd7ff' -shadow 70x6+0+0 \
+                        -channel A -level 0,60% +channel \
+                \) \
+                -background none -compose DstOver -flatten \
+		-channel A -virtual-pixel transparent -morphology Distance Euclidean:8,8! \
+                -resize $w\x$h output/$filename\-mpq-glow-$w.png
 	#exit
 	done
 
--- races/gen.sh
+++ races/gen.sh
@@ -5,12 +5,12 @@
 #
 # Specify size as first argument, final result will be in icons/<size>/
 
-mkdir -p plain
+mkdir -p output
 
 for w in 16 24 32 48 64; do
 	h=$w
 
-	for k in *png; do
+	for k in plain/*png; do
 
 		filename=$(basename "$k")
 		extension="${filename##*.}"
@@ -18,8 +18,29 @@ for w in 16 24 32 48 64; do
 
 		echo "$k -> plain/$filename\-$w.png ($w x $h)"
 
-		convert $k -colorspace Gray -black-threshold 90% -contrast -resize $w\x$h -unsharp 0x1 plain/$filename\-$w.png
-		convert plain/$filename\-$w.png -negate plain/$filename\-$w\-white.png
+		convert $k -colorspace Gray -black-threshold 90% -contrast -resize $w\x$h -unsharp 0x1 output/$filename\-plain\-$w.png
+		convert output/$filename\-plain\-$w.png -negate output/$filename\-plain\-white\-$w.png
+	#exit
+	done
+
+	for k in color/*png; do
+
+		filename=$(basename "$k")
+		extension="${filename##*.}"
+		filename="${filename%.*}"
+
+		echo "$k -> color/$filename\-$w.png ($w x $h)"
+
+		convert $k -resize $w\x$h -unsharp 0x1 output/$filename\-color\-$w.png
+		convert $k \( \
+			+clone -background '#2cd7ff' -shadow 80x10+0+0 \
+			-channel A -level 0,40% +channel \
+		\) \
+		-background none -compose DstOver -flatten \
+		-resize $w\x$h output/$filename\-color-glow-$w.png
+
+		# this is an "inner glow", but it doesn't really look good on small sizes:
+		# -channel A -virtual-pixel transparent -morphology Distance Euclidean:8,8! \
 	#exit
 	done
 done
